a31860b0d349b456e478f29110ea61d54948d197
gadzhi <gadzhi7.7.7@mail.ru>
2019-05-20 17:12:12 +0300

    fixed some styles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,41 +1,42 @@
 <template>
 <div class="api">
-<div class="container-fluid px-5">
-  <div class="row">
-    <ul class="list-group col-md-2 mt-5 pt-3" v-if="movie_list">
-      <li :class="['list-group-item', { 'active': activeItem === category.id } ]" v-for="(category, index) in movie_list.genres" :key="index" @click="loadCard(category.id)">{{category.name|| ''}}</li>
-    </ul>
-    <div class="col-md-10">
-      <div class="d-flex flex-wrap align-content-start justify-content-around my-5 pt-3" v-if="movie_list_id === null && movie_popular">
-        <div class="card mb-3" v-for="(movie, i) in movie_popular.results" :key="i">
-          <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.title">
-          <div class="card-body">
-            <h5 class="card-title">{{movie.title || 'Пусто'}}</h5>
-            <p class="card-text">{{ (movie.overview.substring(0,100) + "...") || ''}}</p>
-            <router-link :to="{ name: 'movie', params: {id: movie.id}}" class="btn btn-primary">Подробнее</router-link>
+  <h1 class="text-center pt-3">Films by API from <span class="text-success">themoviedb</span></h1>
+  <div class="container-fluid px-5">
+    <div class="row">
+      <ul class="list-group col-lg-2 mt-5 pt-3" v-if="movie_list">
+        <li :class="['list-group-item', { 'active': activeItem === category.id } ]" v-for="(category, index) in movie_list.genres" :key="index" @click="loadCard(category.id)">{{category.name|| ''}}</li>
+      </ul>
+      <div class="col-lg-10">
+        <div class="d-flex flex-wrap align-content-start justify-content-around my-5 pt-3" v-if="movie_list_id === null && movie_popular">
+          <div class="card mb-3 col-md-4 col-12" v-for="(movie, i) in movie_popular.results" :key="i">
+            <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.title">
+            <div class="card-body">
+              <h5 class="card-title">{{movie.title || 'Пусто'}}</h5>
+              <p class="card-text">{{ (movie.overview.substring(0,100) + "...") || ''}}</p>
+              <router-link :to="{ name: 'movie', params: {id: movie.id}}" class="btn btn-primary">Подробнее</router-link>
+            </div>
           </div>
         </div>
-      </div>
-      <h1 v-if="movie_list_id" class="text-center my-2">{{movie_list_id.name}}</h1>
-      <div class="d-flex flex-wrap align-content-start justify-content-around" v-if="movie_list_id">
-        <div class="card mb-3" v-for="(movie, i) in movie_list_id.items" :key="i">
-          <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.title">
-          <div class="card-body">
-            <h5 class="card-title">{{movie.title || 'Пусто'}}</h5>
-            <p class="card-text">{{ (movie.overview.substring(0,100) + "...") || ''}}</p>
-            <router-link :to="{ name: 'movie', params: {id: movie.id}}" class="btn btn-primary">Подробнее</router-link>
+        <h1 v-if="movie_list_id" class="text-center my-2">{{movie_list_id.name}}</h1>
+        <div class="d-flex flex-wrap align-content-start justify-content-around" v-if="movie_list_id">
+          <div class="card mb-3 col-md-4 col-12" v-for="(movie, i) in movie_list_id.items" :key="i">
+            <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.title">
+            <div class="card-body">
+              <h5 class="card-title">{{movie.title || 'Пусто'}}</h5>
+              <p class="card-text">{{ (movie.overview.substring(0,100) + "...") || ''}}</p>
+              <router-link :to="{ name: 'movie', params: {id: movie.id}}" class="btn btn-primary">Подробнее</router-link>
+            </div>
           </div>
-        </div>
-        <div class="mt-5" v-if="movie_list_id.items.length === 0">
-          <h3 class="text-center">Фильмы отсутсвуют.</h3>
+          <div class="mt-5" v-if="movie_list_id.items.length === 0">
+            <h3 class="text-center">Фильмы отсутсвуют.</h3>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-<div class="loading" v-if="loading">
-  <h2>Загрузка...</h2>
-</div>
+  <div class="loading" v-if="loading">
+    <h2>Загрузка...</h2>
+  </div>
 </div>
 </template>
 
@@ -68,8 +69,8 @@ export default {
         .get('https://api.themoviedb.org/3/list/' + id + this.api_key)
         .then(response => {
           this.movie_list_id = response.data,
-          this.activeItem = id,
-          this.loading = false
+            this.activeItem = id,
+            this.loading = false
         });
     }
   }
@@ -82,6 +83,10 @@ h5 {
   text-align: center;
 }
 
+li {
+  cursor: pointer;
+}
+
 .loading {
   width: 100%;
   height: 100%;
@@ -89,7 +94,7 @@ h5 {
   align-items: center;
   justify-content: center;
   position: fixed;
-  background-color: rgba(0,0,0, 0.7);
+  background-color: rgba(0, 0, 0, 0.7);
   color: #fff;
   z-index: 999;
   top: 0;
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="container pt-5">
-    <div class="row" v-if="movie">
-      <div class="col-md-4">
-        <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path">
-      </div>
-      <div class="col-md-8">
-        <h1 class="mb-4 text-center">{{movie.title || ''}}</h1>
-        <p><span>Описание:</span> {{movie.overview}}</p>
-        <hr>
-        <p><span>Язык:</span> {{movie.spoken_languages[0].name}}</p>
-        <p><span>Страна:</span> {{movie.production_countries[0].name}}</p>
-        <p><span>Дата выпуска:</span> {{movie.release_date}}</p>
-        <p><span>Популярность:</span> {{movie.popularity}}</p>
-        <p><span>Бюджет:</span> {{movie.budget}}</p>
-        <a href="#video"class="btn btn-primary">Посмотреть видео</a>
-      </div>
+<div class="container pt-5">
+  <div class="text-right mb-4">
+    <router-link :to="{ name: 'home' }" class="btn btn-danger ">На главную страницу</router-link>
+  </div>
+  <div class="row" v-if="movie">
+    <div class="col-md-4">
+      <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path">
     </div>
-    <div class="col-md=12 my-5" id="video" v-if="getVideo">
-      <h2 class="text-center">Видео</h2>
-      <iframe v-for="(video, i) in getVideo.results" width="1280" height="720" :src="'https://www.youtube.com/embed/' + video.key" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div class="col-md-8">
+      <h1 class="mb-4 text-center">{{movie.title || ''}}</h1>
+      <p><span>Описание:</span> {{movie.overview}}</p>
+      <hr>
+      <p><span>Язык:</span> {{movie.spoken_languages[0].name}}</p>
+      <p><span>Страна:</span> {{movie.production_countries[0].name}}</p>
+      <p><span>Дата выпуска:</span> {{movie.release_date}}</p>
+      <p><span>Популярность:</span> {{movie.popularity}}</p>
+      <p><span>Бюджет:</span> {{movie.budget}}</p>
+      <a href="#video" class="btn btn-primary">Посмотреть видео</a>
     </div>
   </div>
+  <div class="col-md=12 my-5" id="video" v-if="getVideo && getVideo.results.length">
+    <h2 class="text-center">Видео</h2>
+    <div class="video__wrapper" v-for="(video, i) in getVideo.results.slice(0, 2)">
+        <iframe :src="'https://www.youtube.com/embed/' + video.key" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div>
+  <div class="loading" v-if="loading">
+    <h2>Загрузка...</h2>
+  </div>
+</div>
 </template>
 
 <script>
@@ -28,17 +36,22 @@ import axios from "axios";
 
 export default {
   name: 'Movie',
-  data () {
+  data() {
     return {
       api_key: '?api_key=4349e092aadf87a170f4f5dd9344058b',
       movie: null,
-      getVideo: null
+      getVideo: null,
+      loading: false
     }
   },
   created() {
+    this.loading = true
     axios
       .get('https://api.themoviedb.org/3/movie/' + this.$route.params.id + this.api_key)
-      .then(response => (this.movie = response.data));
+      .then(response => {
+        this.movie = response.data
+        this.loading = false
+      });
     axios
       .get('https://api.themoviedb.org/3/movie/' + this.$route.params.id + '/videos' + this.api_key)
       .then(response2 => (this.getVideo = response2.data));
@@ -52,5 +65,40 @@ img {
 }
 span {
   font-weight: bold;
+}
+
+.video__wrapper {
+  width: 100%;
+  /* height: 0; */
+  position: relative;
+  padding-bottom: 56.25%;
+  margin-bottom: 20px;
+}
+
+.video__wrapper iframe {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+iframe {
+  width: 100%;
+  height: 100%;
+}
+
+
+.loading {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.1);
+  color: #000;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  transition: 0.3s;
 }
 </style>
