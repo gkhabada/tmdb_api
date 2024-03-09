c32356d81af9b472b1fa3e0421ce8d10b5a65373
gkhabada <gadzhidadaev95@gmail.com>
2024-03-09 17:15:32 +0300

    migrate project to vite

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import Movie from '@/components/Movie'
+import HomeComponent from '../components/HomeComponent.vue'
+import MovieComponent from '../components/MovieComponent.vue'
 
 Vue.use(Router)
 
@@ -10,11 +10,11 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: HomeComponent
     }, {
       path: '/movie/:id',
       name: 'movie',
-      component: Movie
+      component: MovieComponent
     }
   ],
   mode: 'history'
