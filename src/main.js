944d714d2d84e184c14a1b42714bb55a7efaf86e
gkhabada <gadzhidadaev95@gmail.com>
2024-03-11 21:19:27 +0300

    check deploy

--- src/main.js
+++ src/main.js
@@ -4,20 +4,8 @@ import router from './router';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import axios from 'axios'
-// import VueAxios from 'vue-axios'
-
-// Vue.use(VueAxios, axios)
-
 new Vue({
   el: '#app',
   router,
   render: h => h(App),
 })
-
-// new Vue({
-//   el: '#app',
-//   router,
-//   components: { App },
-//   template: '<App/>'
-// })
